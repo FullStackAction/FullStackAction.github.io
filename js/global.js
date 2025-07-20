c0fabc318c3681b3e164721a9a5c5e4611346f73
LinXunFeng <LinXunFeng@users.noreply.github.com>
2025-07-20 08:47:48 +0000

    deploy: FullStackAction/vuepress-blog@ad1e355d383acb881d5f48d7a321a2c29a48d154

--- js/global.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    var footer = document.getElementsByClassName("footer")[0]
-    console.log(footer.childNodes)
-    footer.childNodes[1].nodeValue = '微信公众号 '
-    footer.childNodes[2].href = 'https://cdn.jsdelivr.net/gh/FullStackAction/PicBed@resource/image/20210131111432.png'
-    footer.childNodes[2].innerHTML = 'FullStackAction'
-}
